40305c73fb7c2a07125e3af92b5d883e819db925
msneddon <mwsneddon@lbl.gov>
2014-06-04 10:36:12 -0700

    added metadata annotation test

--- src/us/kbase/kidl/test/KidlTest.java
+++ src/us/kbase/kidl/test/KidlTest.java
@@ -274,7 +274,7 @@ public class KidlTest {
 	@Test
 	public void testJsonSchemas2() throws Exception {
 		boolean ok = true;
-		for (int testNum = 1; testNum <= 21; testNum++) {
+		for (int testNum = 1; testNum <= 22; testNum++) {
 			if (testNum == 9) {
 				continue;
 			}
