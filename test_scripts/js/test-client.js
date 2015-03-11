b817b4ffd53b067903e63d5075ebd5dc12f9ad41
msneddon <mwsneddon@lbl.gov>
2015-03-11 13:15:53 -0700

    js test script working

--- test_scripts/js/test-client.js
+++ test_scripts/js/test-client.js
@@ -1,11 +1,12 @@
 //
 // usage:
-//   casperjs test test-client.js --fail-fast --tests=[test_cfg_file] --endpoint=[server_url] --token=[token]
+//   casperjs test test-client.js --tests=[test_cfg_file] --endpoint=[server_url] --token=[token]
 //
 //
 
 
-casper.test.begin('JS Client Tests', function suite(test) {
+
+casper.test.begin('JS Client Test Initialization', 8, function suite(test) {
 
     // First test that we have all the arguments we need
     test.assertTruthy(casper.cli.get('tests'),
@@ -13,8 +14,8 @@ casper.test.begin('JS Client Tests', function suite(test) {
     test.assertTruthy(casper.cli.get('endpoint'),
 		'Argument "endpoint" must be defined and set to service url.');
 
-    //test.assertTruthy(casper.cli.get('token'),
-    //	'Argument "token" must be defined and set to a valid auth token.');
+    test.assertTruthy(casper.cli.get('token'),
+    	'Argument "token" must be defined and set to a valid auth token.');
 
     // Read the config and make sure we have the right things
     var fs = require('fs');
@@ -35,59 +36,101 @@ casper.test.begin('JS Client Tests', function suite(test) {
     // instantiate the clients
     var noauthClient = new window[testcfg['class']](casper.cli.get('endpoint'));
     test.assertTruthy(noauthClient,'Unauthenticated client instantiated.');
-    var authClient   = new window[testcfg['class']](casper.cli.get('endpoint'),{});
+    var authClient   = new window[testcfg['class']](casper.cli.get('endpoint'),{token:casper.cli.get('token')});
     test.assertTruthy(noauthClient,'Authenticated client instantiated.');
 
     // run through each test
-    var tests = testcfg.tests;
+    var tests = testcfg.tests; var callsReturned = 0; var callsExpected = 0;
     for(var t=0; t<tests.length; t++) {
         var client = noauthClient;
-        if(tests[t].auth) {
-            client=authClient;
+        if(tests[t].auth) { client=authClient; }
+
+        // we don't actually need these tests- if the method doesn't exist we will fail later
+        if(!client.hasOwnProperty(tests[t].method)) {
+            test.fail(tests[t].method+' is not a defined function in the client!')
         }
+        if(typeof client[tests[t].method] !== 'function') {
+           test.fail(tests[t].method+' is not actually a function!  It is a '+(typeof client[tests[t].method]));
+        }
+        //test.assert(client.hasOwnProperty(tests[t].method),'--'+tests[t].method+' is defined --');
+        //test.assert(typeof client[tests[t].method] === 'function',tests[t].method+' is actually a function');
 
-        test.assert(client.hasOwnProperty(tests[t].method),'--------- '+tests[t].method+' is defined ----------');
-        test.assert(typeof client[tests[t].method] === 'function',tests[t].method+' is actually a function');
-        //console.log(noauthClient[tests[t].method]);
-        
-        var promise = client[tests[t].method](tests[t].params);
-        console.log(promise);
-        promise.done(function(result) {
-            console.log('here');
-        })
-        /*casper.test.begin('test method', function suite(test){
+        // Make the async call to the server; use a function to get the right closure
+        (function(client, method, params, outcome) {
+            callsExpected = callsExpected +1;
 
-            client[tests[t].method](tests[t].params,
+            // parameters include the function that handles success and error
+            var sent = null;
+            if(params.length===1) {
+                sent = params[0];  // handle case with one returned object
+            } else if(params.length>1) {
+                sent = []; // handle case where we expect multiple returned objects
+                for(var k=0; k<params.length; k++) { sent.push(params[k]); }
+            }
+            params.push(
                 function(result) {
-                    console.log(result);
-                    test.done();
-                },
-                function(err) {
-                    console.log('got error:',err);
-                    test.done();
+                    // note: not sure if this is the right way to do this, but had to create another begin block
+                    // to properly handle the async test responses
+                    casper.test.begin('Test Method Response: "'+method+'"', 2, function suite(test) {
+                        if(outcome.status==='pass') {
+                            test.pass("Expected to run successfully, and did");
+                            test.assertEquals(result, sent, 'method input should match method output');
+                        } else if(outcome.status==='nomatch') {
+                            test.pass("Expected to run successfully, and did");
+                            test.assertNotEquals(result, sent, 'method input should match method output');
+                        } else if(outcome.status==='fail') {
+                            test.fail("Expected to fail, but the method returned success");
+                        } else {
+                            test.fail("Unexpected outcome defined: '"+outcome.status+"' - support only for pass | fail | nomatch");
+                        }
+                        test.done();
+                    });
+
+                    // finish up the main test if all the calls returned
+                    callsReturned = callsReturned +1;
+                    if(callsReturned===callsExpected) { test.done(); }
                 });
-        });*/
+            params.push(
+                function(err) {
+                    casper.test.begin('Test Method Response: "'+method+'"',1, function suite(test) {
+                        if(outcome.status==='fail') {
+                            test.pass("Expected to fail, and did");
+                            if(!err.error) {
+                                test.fail("err.error not defined in returned error object");
+                                if(!err.error.message) {
+                                    test.fail("err.error.message not defined in returned error object");
+                                }
+                            }
 
-        //console.log(JSON.stringify(tests[t],null, 2));
+                            if(outcome.error) {
+                                for(var k=0; k<outcome.error.length; k++) {
+                                    if(err.error.message.indexOf(outcome.error[k])<0) {
+                                        test.fail("error message should contain: '"+ outcome.error[k]+
+                                            "' but did not. \nError was:\n"+err.error.message);
+                                    }
+                                }
+                            }
+
+
+                        } else if(outcome.status==='nomatch') {
+                            test.fail("Expected to fail, but ran successfully");
+                        } else if(outcome.status==='pass') {
+                            test.fail("Expected to fail, but ran successfully");
+                        } else {
+                            test.fail("Unexpected outcome defined: '"+outcome.status+"' - support only for pass | fail | nomatch");
+                        }
+                        test.done();
+                    });
+
+                    // finish up if all the calls returned
+                    callsReturned = callsReturned +1;
+                    if(callsReturned===callsExpected) { test.done(); }
+                });
+
+            console.log('# submitting async call for '+method);
+            client[method].apply(this,params);
+        })(client, tests[t].method, tests[t].params, tests[t].outcome);
     }
-
-	/*casper.test.begin('test method', function suite(test){
-
-		casper.start(casper.cli.get("tests"), function() {console.log('got_something')}); 
-
-
-		test.assertEquals(1, 1);
-	    test.done();
-
-
-	});*/
-
-    //test.done();
-
 });
-	
 
 
-
-
-//casper.test.done();
